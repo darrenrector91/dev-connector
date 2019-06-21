f2d37ac6aae886a8c813c39ada107147d15b728c
darrenrector91 <darrenrector91@gmail.com>
2019-06-20 22:23:58 -0500

    added logout functionality

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,5 @@
+export const GET_ERRORS = 'GET_ERRORS';
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
@@ -6,3 +8,9 @@ export const AUTH_ERROR = 'AUTH_ERROR';
 export const USER_LOADED = 'USER_LOADED';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
+export const CLEAR_CURRENT_PROFILE = 'CLEAR_CURRENT_PROFILE';
+export const GET_PROFILE = 'GET_PROFILE';
+export const GET_PROFILES = 'GET_PROFILES';
+export const POST_LOADING = 'POST_LOADING';
+export const PROFILE_LOADING = 'PROFILE_LOADING';
--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -2,6 +2,7 @@ import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 const initialState = [];
 
 export default function(state = initialState, action) {
+  // eslint-disable-next-line
   const { type, payload } = action;
 
   switch (action.type) {
